3bcc400bd7fa3f8f7db6bdf3a0eea43e0c239df4
Ben Gallagher <ben.gallagher@immersivelas.com>
2024-11-07 14:40:57 +0000

    Comment redeployment trigger

--- module/ecs-service.tf
+++ module/ecs-service.tf
@@ -7,13 +7,13 @@ resource "aws_ecs_service" "this" {
 
   # force deployment update on any terraform plan (useful for testing)
   force_new_deployment = true
-  triggers = {
-    redeployment = plantimestamp()
-  }
+  # triggers = {
+  #   redeployment = plantimestamp()
+  # }
 
   network_configuration {
     subnets          = module.vpc.public_subnets
-    security_groups  = [aws_security_group.this.id]
+    security_groups  = [aws_security_group.ecs.id]
     assign_public_ip = true
   }
 
